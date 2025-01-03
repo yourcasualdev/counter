6ff53e54afd379226e1d2fa410b13f53c609ab4b
yourcasualdev <babalibrahim34@gmail.com>
2025-01-03 16:44:20 +0300

    Update project configuration: add new environment files to .gitignore, include @upstash/redis dependency in package.json, and modify data fetching in index.js to use the new API route. Update yarn.lock accordingly.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-
 export default function Home({ data }) {
   return (
     <div>
-      <h1 className="title">How many times visited ?</h1>
+      <h1 className="title">How many times visited?</h1>
       <div className="block glow">
         <p>{data}</p>
       </div>
@@ -11,11 +10,10 @@ export default function Home({ data }) {
 }
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://how-many-times-clicked.herokuapp.com/num`)
-  const data = await res.json()
-
+  // Fetch data from our API route
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/views`);
+  const data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
 }
