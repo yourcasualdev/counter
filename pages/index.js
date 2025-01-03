f0fb7cb3c01c4fd5b16e569e7bd8ce355a785f53
yourcasualdev <babalibrahim34@gmail.com>
2025-01-03 17:27:17 +0300

    Remove unused global styles, layout, and view count API; downgrade Next.js version in package.json for compatibility

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+export default function Home({ data }) {
+  return (
+    <div>
+      <h1 className="title">How many times visited?</h1>
+      <div className="block glow">
+        <p>{data}</p>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  // Using absolute URL since relative URL wasn't working
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/views`);
+  const data = await res.json();
+
+  return { props: { data } };
+}
