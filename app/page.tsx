2139e209f790ab180dbd52edea9ca038430a491e
yourcasualdev <babalibrahim34@gmail.com>
2025-01-03 17:19:44 +0300

    Refactor data fetching URL in page.tsx to use NEXT_PUBLIC_URL environment variable for improved consistency and reliability

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 async function getViewCount() {
-  const res = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/views`,
-    {
-      cache: "no-store",
-    }
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/views`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch view count");
